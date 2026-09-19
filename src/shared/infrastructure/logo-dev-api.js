33b3dbdb573d9aee1fa5e9736373dc2b87bdfff7
Efrain Bautista <efrain@thibau.io>
2026-09-19 07:18:32 -0500

    feat(shared): add Logo.dev API helper.

---
+++ src/shared/infrastructure/logo-dev-api.js
@@ -0,0 +1,6 @@
+const logoApiUrl = import.meta.env.VITE_LOGO_API_URL;
+const apiKey = import.meta.env.VITE_LOGO_PUBLISHABLE_API_KEY;
+
+export class LogoDevApi {
+    getUrlToLogo = url => `${logoApiUrl}/${new URL(url.toString()).host}?token=${apiKey}`;
+}
