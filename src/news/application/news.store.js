67a7b8e9b608dfa65b7a49a2cfb954196082b735
Efrain Bautista <efrain@thibau.io>
2026-09-23 07:23:12 -0500

    feat(news): load articles for the current source.

--- src/news/application/news.store.js
+++ src/news/application/news.store.js
@@ -1,17 +1,28 @@
 import {computed, ref, shallowRef} from "vue";
 import {NewsApi} from "../infrastructure/news-api.js";
 import {SourceAssembler} from "../infrastructure/source.assembler.js";
+import {ArticleAssembler} from "../infrastructure/article.assembler.js";
 
 class NewsStore {
     #newsApi = new NewsApi();
     #sourceAssembler = new SourceAssembler();
     #sources = shallowRef([]);
+    #articles = shallowRef({});
     #currentSource = shallowRef(undefined);
     #errors = ref([]);
     #currentSourceId = computed(() => this.#currentSource.value?.id);
 
+    #currentSourceArticles = computed(() => {
+        const id = this.#currentSourceId.value;
+        return id ? (this.#articles.value[id] ?? []) : [];
+    });
+
     get sources() {
         return this.#sources.value;
+    }
+
+    get articles() {
+        return this.#articles.value;
     }
 
     get errors() {
@@ -26,8 +37,13 @@ class NewsStore {
         return this.#currentSourceId.value;
     }
 
+    get currentSourceArticles() {
+        return this.#currentSourceArticles.value;
+    }
+
     setCurrentSource(source) {
         this.#currentSource.value = source;
+        this.loadArticlesForCurrentSource();
     }
 
     loadSources() {
@@ -40,6 +56,18 @@ class NewsStore {
         }).catch(message => {
             this.#errors.value.push(message);
             this.#sources.value = [];
+        });
+    }
+
+    loadArticlesForCurrentSource() {
+        const source = this.#currentSource.value;
+        if (!source) return;
+        if (this.#articles.value[source.id]) return;
+        this.#newsApi.getArticlesForSourceId(source.id).then(response => {
+            const articles = new ArticleAssembler(source).toEntitiesFromResponse(response);
+            this.#articles.value = {...this.#articles.value, [source.id]: articles};
+        }).catch(message => {
+            this.#errors.value.push(message);
         });
     }
 }
