7bbca6b0af0997eb71ecbafc94615d14fc07e36d
Efrain Bautista <efrain@thibau.io>
2026-09-23 07:53:03 -0500

    feat(news): add article-list component.

---
+++ src/news/presentation/components/article-list.vue
@@ -0,0 +1,16 @@
+<script setup lang="js">
+import ArticleItem from "./article-item.vue";
+
+const { articles } = defineProps({ articles: { type: Array, required: true } });
+
+</script>
+
+<template>
+  <div v-for="article in articles" :key="article.url.toString()">
+    <article-item :article="article"/>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
