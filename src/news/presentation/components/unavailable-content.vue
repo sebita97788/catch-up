f291c92835b42d9e3234ed45dfde87e56eea93c2
Efrain Bautista <efrain@thibau.io>
2026-09-23 08:28:36 -0500

    feat(news): add unavailable-content component.

---
+++ src/news/presentation/components/unavailable-content.vue
@@ -0,0 +1,21 @@
+<script setup lang="js">
+import {useI18n} from "vue-i18n";
+
+const { t } = useI18n();
+
+const { errors } = defineProps({ errors: { type: Array, default: () => [] } });
+</script>
+
+<template>
+  <div class="flex flex-column align-items-center justify-content-center mt-8 text-muted-color">
+    <i class="pi pi-exclamation-circle text-6xl mb-4" />
+    <div><h4>{{ t('unavailable-news') }}</h4></div>
+    <div v-for="error in errors" :key="error" class="mt-2">
+      <h6>{{ error }}</h6>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
